4a3c359902c5bc5425d44ca37c9efea8c14c70c5
Mack Maier <hmaiermack@gmail.com>
2020-03-23 17:22:30 -0700

    finished server with query options, authorization, and cors/helmet

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 const express = require('express')
 const movies = require('./movies-data-small.json')
+const helmet = require('helmet')
+const cors = require('cors')
+require('dotenv').config()
+
 
 const app = express();
 
+app.use(helmet())
+app.use(cors())
+console.log(process.env.API_TOKEN)
+
+
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization')
+    console.log(authToken)
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+  })
+
 app.get('/movie', (req, res)=> {
-    res.json(movies)
+    const {genre, country, avg_vote} = req.query;
+    let data = movies;
+
+    if(genre){
+        data = data.filter(movie => {
+            return movie.genre.toLowerCase().includes(genre.toLowerCase())
+        })
+    }
+
+    if(country){
+        data = data.filter(movie => {
+            return movie.country.toLowerCase().includes(country.toLowerCase())
+        })
+    }
+
+    if(avg_vote){
+        data = data.filter(movie => {
+            if(Number(movie.avg_vote) >= Number(avg_vote)){
+                return movie;
+            }
+        })
+    }
+
+
+    res.json(data)
 })
 
 app.listen(8000, ()=> {
